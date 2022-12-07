3cbdcea7c4701ea03317129d48cdea094ed352f6
frenude <frenude@gmail.com>
2022-12-07 16:19:45 +0800

    fix bugs

--- service/calculate.go
+++ service/calculate.go
@@ -8,6 +8,9 @@ import (
 
 func CalculateNextPoint(ctx context.Context, snake, ladder [][]int, chess *vo.Chess) int {
 	s := chess.Point + chess.Move
+	if s >= 100 {
+		return 100 - (s - 100)
+	}
 	for i := 0; i < len(snake); i++ {
 		inSlice := snake[i]
 		sort.Sort(sort.Reverse(sort.IntSlice(inSlice)))
